b320e358fc74be8cca789f311112db7137a0fc00
Deep <smarreddy1@gmail.com>
2014-01-29 13:08:01 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JdSite::Application.config.secret_key_base = 'e213165b57441c20f178b6c60fb84ca355d86c1146abfc392307206b81e9026a0cc6ef34c3ed46dd8c9868d5e1b9c6c56e8ff472b0c3ac9b220538e97c179f9a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JdSite::Application.config.session_store :cookie_store, key: '_jd_site_session'
